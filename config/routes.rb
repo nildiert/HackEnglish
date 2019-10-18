439df5796614931f3ffb875fa2fb56c6201d5933
nildiert <niljordan23@gmail.com>
2019-10-18 18:17:20 -0500

    [FEATURE] Added roles controller, routes and factories

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/health', to: 'health#health'
+
+  resources :roles, only: [:index, :show, :create, :update, :destroy]
+
+
+  # achievement 
+  # level
+  # concept
+  # project
+  # task
+  # check
+  # user
+  # user_achievement
+  # user_project
+  # task_assignment
+  # check_task_assignment
+  # 
 end
