6cb424697bba96b8891e385217036f87c8ad7541
Alejo-Rey <771@holbertonschool.com>
2019-10-19 00:02:09 +0000

    [FEATURE] adds routes and controller Achievements and FABRICBOT

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :achievements, only: [:index]
+
   get '/health', to: 'health#health'
 
   resources :roles, only: [:index, :show, :create, :update, :destroy]
-
-
-  # achievement 
+  resources :achievements, only: [:index, :show, :create, :update, :destroy]
   # level
   # concept
   # project
