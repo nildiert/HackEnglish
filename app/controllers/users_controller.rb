eaef4207da2d7b83e492bc28fabfa962516ece8d
diegoorejuela <dfrodriguezor@gmail.com>
2019-10-19 03:59:47 +0000

    [FEATURE] method post user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,15 @@ class UsersController < ApplicationController
    
    
     # POST /users
-
    def create
-        @user = User.create(create_params)
-        render json: @user, status: :created
+     @user = User.create(create_params)
+     render json: @user, status: :created
    end
 
    private
    
    def create_params
-        params.require(:user).permit(:id_rol, :status, :email, :password, :first_name, :last_name, :profile_pic, :description, :score)
+        params.require(:user).permit(:role_id, :status, :email, :password, :first_name, :last_name, :profile_pic, :description, :score)
    end
 
 end
