5505c1ca8f04e176623cf9e2f0ad4d2e4327fe6a
diegoorejuela <dfrodriguezor@gmail.com>
2019-10-20 08:07:24 +0000

    [feature] many to many relations user<->projects

---
+++ destroy_models.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rails destroy model role
+rails destroy model achievement
+rails destroy model level
+
+rails destroy model project
+rails destroy model task
+rails destroy model check
+rails destroy model user
+
+rails destroy model concept
+rails destroy model user_achievement
+rails destroy model user_project
+rails destroy model task_assignment
+rails destroy model check_task_assignment
