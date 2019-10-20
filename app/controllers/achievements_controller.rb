e66a80e2bc6c42a7e215c00f4635977fc8ce53e0
Alejo-Rey <771@holbertonschool.com>
2019-10-20 02:33:01 +0000

    [FIX] commits route in methos PUTS

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -24,7 +24,7 @@ class AchievementsController < ApplicationController
         render json: @achievements, status: :created
     end
 
-    # PUT /roles/{id}
+    # PUT /achievements/{id}
     def update
         @achievements = Achievement.find(params[:id])
         @achievements.update(update_params)
