b93d2a628df36edba0176a27d39937f5c81e27af
diegoorejuela <dfrodriguezor@gmail.com>
2019-10-27 00:38:13 +0000

    [FEATURE] generate token implement

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   has_many :projects, through: :userProjects
   has_many :userAchievements
   has_many :achievements, through: :userAchievements
+
+  after_initialize :generate_auth_token
+
+  def generate_auth_token
+    unless auth_token.present?
+      self.auth_token = ::TokenGenerationService.generate
+    end
+  end
 end
