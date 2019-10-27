1194262575e0e44a61369c0b8ea5b5137c48ec4e
diegoorejuela <dfrodriguezor@gmail.com>
2019-10-27 08:12:36 +0000

    [FEATURE] serializer user_info_level_concepts_controller

---
+++ app/controllers/user_info_level_concepts_controller.rb
@@ -0,0 +1,11 @@
+class UserInfoLevelConceptsController < ApplicationController
+    
+  include Secured    
+  protect_from_forgery prepend: true
+  before_action :authenticate_user!, only: [:index, :show, :create, :update, :destroy]
+
+    def index
+      @user = User.find(params[:id])
+      render json: @user, serializer: UserInfoLevelConcepts 
+    end
+end
